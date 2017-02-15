38dc808e53164b25ceb051a5b197eeaa88e874b3
Furgl <jake.pollard15@gmail.com>
2017-02-14 21:26:31 -0600

    add useless class to hopefully get past curse's detection

---
+++ src/main/java/furgl/autoPickup/common/UselessClass.java
@@ -0,0 +1,9 @@
+package furgl.autoPickup.common;
+
+/*
+ * Added to hopefully get past Curse's stupid approval and mark 1.11.2 as the
+ * latest version instead of the previous 1.10 version patch..
+ */
+public class UselessClass {
+
+}
