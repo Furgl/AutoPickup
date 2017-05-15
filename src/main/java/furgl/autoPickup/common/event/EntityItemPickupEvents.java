8fe75d6297aafe429f61f130e9943382b46d7423
Furgl <jake.pollard15@gmail.com>
2017-05-14 19:58:19 -0500

    v2.4.3

--- src/main/java/furgl/autoPickup/common/event/EntityItemPickupEvents.java
+++ src/main/java/furgl/autoPickup/common/event/EntityItemPickupEvents.java
@@ -9,11 +9,13 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class EntityItemPickupEvents {
 	@SubscribeEvent(priority=EventPriority.NORMAL, receiveCanceled=true)
 	public void onEvent(EntityItemPickupEvent event) {
-		Config.syncFromConfig(event.getEntityPlayer().getName());
-		if (!AutoPickup.key.isKeyDown(event.getEntityPlayer()) && 
-				Config.blacklistNames.contains(event.getItem().getEntityItem().getItem().getItemStackDisplayName(event.getItem().getEntityItem()).replace(" ", "_"))) {
-			event.getItem().setDefaultPickupDelay();
-			event.setCanceled(true);		
+		if (!event.getItem().getEntityData().getBoolean("Fake Item")) {
+			Config.syncFromConfig(event.getEntityPlayer().getName());
+			if (!AutoPickup.key.isKeyDown(event.getEntityPlayer()) && 
+					Config.blacklistNames.contains(event.getItem().getEntityItem().getItem().getItemStackDisplayName(event.getItem().getEntityItem()).replace(" ", "_"))) {
+				event.getItem().setDefaultPickupDelay();
+				event.setCanceled(true);		
+			}
 		}
 	}
 }
